7cb6a28cd5962bee5aed6995950f15743d99c6f7
Ruth Acosta <ruthacosta100@gmail.com>
2017-04-23 22:54:18 -0400

    second commit

---
+++ js/andy.js
@@ -0,0 +1,7 @@
+<script src="ideal-image-slider.js"></script>
+<script src="extensions/captions/iis-captions.js"></script>
+<script>
+var slider = new IdealImageSlider.Slider('#slider');
+slider.addCaptions();
+slider.start();
+</script>
